df5c05aa1fe17447d8759f4f57b1f0fe23ddcc26
npstr <napstr@users.noreply.github.com>
2016-08-10 01:19:44 +0200

    fixes a start up error when running with mono

--- PokemonGo.RocketAPI/HttpClient/PokemonClient.cs
+++ PokemonGo.RocketAPI/HttpClient/PokemonClient.cs
@@ -6,13 +6,29 @@ namespace PokemonGo.RocketAPI.HttpClient
 {
     public class PokemonHttpClient : System.Net.Http.HttpClient
     {
-        private static readonly HttpClientHandler Handler = new HttpClientHandler
+        /*private static readonly HttpClientHandler Handler = new HttpClientHandler
         {
             AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
             AllowAutoRedirect = false,
             UseProxy = Client.Proxy != null,
             Proxy = Client.Proxy
-        };
+        };*/
+        
+        //fix below from https://github.com/NECROBOTIO/NecroBot/issues/3289#issuecomment-238615898
+        static PokemonHttpClient()
+        {
+            Handler = new HttpClientHandler();
+
+            Handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            Handler.AllowAutoRedirect = false;
+
+            if (Client.Proxy != null)
+            {
+                Handler.UseProxy = true;
+                Handler.Proxy = Client.Proxy;
+            }
+        }
+        private static readonly HttpClientHandler Handler;
 
         public PokemonHttpClient() : base(new RetryHandler(Handler))
         {
